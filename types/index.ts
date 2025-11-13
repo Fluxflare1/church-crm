e861623855c85040e4164a01f7369d9e8783414b
Itebimien Momodu <fluxflareltd@gmail.com>
2025-11-13 15:55:17 +0100

    Create index.ts

---
+++ types/index.ts
@@ -0,0 +1,14 @@
+// types/index.ts
+
+export * from './auth';
+export * from './users';
+export * from './people';
+export * from './programs';
+export * from './attendance';
+export * from './tally';
+export * from './follow-up';
+export * from './communications';
+export * from './broadcasts';
+export * from './notifications';
+export * from './analytics';
+export * from './config';
